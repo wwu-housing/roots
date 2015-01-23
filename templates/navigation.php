e422335a7194c2d7126886c731a783390ecd5409
Cameron Little <Cameron.Little@wwu.edu>
2015-01-23 15:58:27 -0800

    Modified navwalker to simulate our navigation structure.

--- templates/navigation.php
+++ templates/navigation.php
@@ -5,13 +5,13 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
     </button>
-    <nav id="main-nav" class="container collapse {% block nav_collapse %}in{% endblock %}">
+    <nav id="main-nav" class="container collapse in">
         <span class="sr-only visible-xs">Use enter and backspace to navigate between menu levels, tab and shift+tab to navigate between items in a level</span>
         <button class="back btn btn-ur disabled visible-xs"><span class="sr-only">Back</span><i class="glyphicon glyphicon-arrow-left glyphicon glyphicon-white"></i></button>
         <div id="ur-nav" class="row nav-primary" role="navigation">
         <?php
             if (has_nav_menu('primary_navigation')) :
-                wp_nav_menu(array('theme_location' => 'primary_navigation', 'walker' => new Roots_Nav_Walker(), 'menu_class' => 'nav navbar-nav'));
+                wp_nav_menu(array('theme_location' => 'primary_navigation', 'walker' => new Roots_Nav_Walker(), 'menu_class' => 'nav simple'));
             endif;
         ?>
         </div>
